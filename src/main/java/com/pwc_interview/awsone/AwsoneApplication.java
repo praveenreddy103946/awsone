f9b5cc8cea953c9f9032f56c83f81f9196786bcc
Ippa Bharath <bharathippa02@gmail.com>
2025-02-07 12:01:31 +0530

    first aws commit

---
+++ src/main/java/com/pwc_interview/awsone/AwsoneApplication.java
@@ -0,0 +1,30 @@
+package com.pwc_interview.awsone;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("aws")
+@Slf4j
+public class AwsoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsoneApplication.class, args);
+	}
+
+
+	@GetMapping("/prawin")
+	public String testaws(){
+		System.out.println("Inside test aws method");
+		log.info("checking logs for path /prawin");
+		return "application successfully deployed to aws";
+
+
+	}
+
+}
